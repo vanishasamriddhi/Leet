3e8e7553bf4d244e32a4a779a7aeac5b477afbcb
Vanisha Samriddhi <71593673+vanishasamriddhi@users.noreply.github.com>
2024-09-21 16:47:04 +0530

    Time: 4 ms (55.66%), Space: 14.1 MB (86.93%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -12,32 +12,36 @@
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        map <int, map<int, multiset <int>>> nodes;
-        queue <pair<TreeNode* ,pair <int,int>>> todo;
-        todo.push({root,{0,0}});
-        while(!todo.empty()){
-            auto p= todo.front();
-            todo.pop();
-            TreeNode* node= p.first;
-            int x= p.second.first, y = p.second.second;
-            nodes[x][y].insert(node->val);
-            if(node->left){
-                todo.push({node->left, {x-1,y+1}});
-            }
-            if(node->right){
-                todo.push({node->right, {x+1,y+1}});
-            }
-            
+        map<int, map<int, vector<int>>> nodes; // Change to vector instead of multiset
+        queue<pair<TreeNode*, pair<int, int>>> todo;
+        todo.push({root, {0, 0}});
         
+        while (!todo.empty()) {
+            auto p = todo.front();
+            todo.pop();
+            TreeNode* node = p.first;
+            int x = p.second.first, y = p.second.second;
+            
+            nodes[x][y].push_back(node->val); // Insert into vector
+            
+            if (node->left) {
+                todo.push({node->left, {x - 1, y + 1}});
+            }
+            if (node->right) {
+                todo.push({node->right, {x + 1, y + 1}});
+            }
         }
-        vector <vector<int>> ans;
-        for(auto p:nodes){
-        vector <int> col;
-            for(auto q: p.second){
+        
+        vector<vector<int>> ans;
+        for (auto& p : nodes) {
+            vector<int> col;
+            for (auto& q : p.second) {
+                sort(q.second.begin(), q.second.end()); // Sort vector elements
                 col.insert(col.end(), q.second.begin(), q.second.end());
             }
             ans.push_back(col);
         }
+        
         return ans;
     }
 };
